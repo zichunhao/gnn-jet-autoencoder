3ecf870979628f2de0c077b4f8c88fcc3d9c10a1
Zichun Hao <zichunh2000@gmail.com>
2022-10-21 22:08:33 -0700

    Update docs

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # GNN Jet Autoencoder
 A GNN autoencoder for jets in particle physics implemented in PyTorch.
+
+## Data
+To download data:
+1. Install `JetNet`:
+    ```
+    pip3 install jetnet; 
+    ```
+2. Run `preprocess.py`
+    ```
+    python utils/data/preprocess.py \
+    --jet-types g q t w z \
+    --save-dir "./data"
+    ```
+
+## Training
+To train the model, run `train.py`. An example is provided in `examples/main.sh`.
---
+++ examples/main.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p "dev";
+python main.py \
+--batch-size 16 \
+--jet-type qcd \
+--data-paths "data/g_jets_30p_small.pt" "data/q_jets_30p_small.pt" \
+--encoder-edge-sizes '16,16,8,8;' \
+--decoder-edge-sizes '16,16,8,8;' \
+--encoder-node-sizes '3;3;3;3;' \
+--decoder-node-sizes '3;3;3;3;' \
+--save-dir dev \
+--num-epochs 10 \
+--latent-map "local mix" \
+--latent-node-size 2
+--train-fraction 0.65 \
+--save-dir dev \
+| tee -a dev/autoencoder-g-s1-v1.txt
