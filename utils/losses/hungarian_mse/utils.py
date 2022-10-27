8e6646a26a5c59ae3ec96527daaa1c33768524f3
Zichun Hao <zichunh2000@gmail.com>
2022-10-26 23:04:05 -0700

    [FIX] transformation to polar_rel

--- utils/losses/hungarian_mse/utils.py
+++ utils/losses/hungarian_mse/utils.py
@@ -1,12 +1,8 @@
+import math
 import torch
 
 
-def get_eps(p):
-    dtype = p.dtype
-    if dtype is [torch.float64, torch.double]:
-        return 1e-16
-    else:
-        return 1e-12
+EPS = 1e-16
 
 
 def get_p_polar(p):
@@ -14,23 +10,18 @@ def get_p_polar(p):
     (E, px, py, pz) or (px, py, pz)-> (eta, phi, pt)
     """
     if p.shape[-1] == 4:
-        idx_px, idx_py, idx_pz = 1, 2, 3
+        _, px, py, pz = p.unbind(-1)
     elif p.shape[-1] == 3:
-        idx_px, idx_py, idx_pz = 0, 1, 2
+        px, py, pz = p.unbind(-1)
     else:
         raise ValueError(f'Wrong last dimension of p. Should be 3 or 4 but found: {p.shape[-1]}.')
 
-    px = p[..., idx_px]
-    py = p[..., idx_py]
-    pz = p[..., idx_pz]
-    eps = get_eps(p)
-
-    pt = torch.sqrt(px ** 2 + py ** 2 + eps)
+    pt = torch.sqrt(px ** 2 + py ** 2 + EPS)
     try:
-        eta = torch.asinh(pz / (pt + eps))
+        eta = torch.asinh(pz / (pt + EPS))
     except AttributeError:
-        eta = arcsinh(pz / (pt + eps))
-    phi = torch.atan2(py + eps, px + eps)
+        eta = arcsinh(pz / (pt + EPS))
+    phi = torch.atan2(py + EPS, px + EPS)
 
     return torch.stack((pt, eta, phi), dim=-1)
 
@@ -44,29 +35,24 @@ def get_polar_rel(p, jet, input_cartesian=True):
         p = get_p_polar(p)  # Convert to polar first
         jet = get_p_polar(jet)
 
-    pt = p[..., 0]
-    eta = p[..., 1]
-    phi = p[..., 2]
+    pt, eta, phi = p.unbind(-1)
 
     num_particles = p.shape[-2]
-    pt /= jet[..., 0].unsqueeze(dim=-1).repeat(1, num_particles)
+    pt /= (jet[..., 0] + EPS).unsqueeze(dim=-1).repeat(1, num_particles)
     eta -= jet[..., 1].unsqueeze(dim=-1).repeat(1, num_particles)
     phi -= jet[..., 2].unsqueeze(dim=-1).repeat(1, num_particles)
+    phi = (phi + math.pi) % (2 * math.pi) - math.pi
 
     return torch.stack((pt, eta, phi), dim=-1)
 
 
 def get_p_cartesian(p, return_p0=False):
     if p.shape[-1] == 4:
-        idx_pt, idx_eta, idx_phi = 1, 2, 3
+        _, pt, eta, phi = p.unbind(-1)
     elif p.shape[-1] == 3:
-        idx_pt, idx_eta, idx_phi = 0, 1, 2
+        pt, eta, phi = p.unbind(-1)
     else:
         raise ValueError(f'Wrong last dimension of p. Should be 3 or 4 but found: {p.shape[-1]}.')
-
-    pt = p[..., idx_pt]
-    eta = p[..., idx_eta]
-    phi = p[..., idx_phi]
 
     px = pt * torch.cos(phi)
     py = pt * torch.cos(phi)
