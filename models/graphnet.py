46819ececcda4b096b915a52d88ca76beceb5cb3
Zichun Hao <zichunh2000@gmail.com>
2022-10-26 22:25:16 -0700

    Refactor by removing uncessary parameters

--- models/graphnet.py
+++ models/graphnet.py
@@ -1,4 +1,4 @@
-from typing import Callable, List, Union
+from typing import Callable, Iterable, List, Union
 import warnings
 import torch
 import torch.nn as nn
@@ -137,7 +137,7 @@ class GraphNet(nn.Module):
 
         :param x: Input node features.
         :type x: torch.Tensor
-        :param metric: Metric to compute the distance, defaults to 'cartesian'. 
+        :param metric: Metric to compute the distance, defaults to 'euclidean'. 
         Choice: ('Minkowskian', 'euclidean'). 
         'Minkowskian' is only available for 4-vectors. 
         :type metric: str, optional
@@ -146,14 +146,24 @@ class GraphNet(nn.Module):
         """
         self.metric = metric.lower()
         batch_size = x.shape[0]
-        x = x.to(device=self.device, dtype=self.dtype)
+        x = x.to(device=self.device, dtype=self.dtype)  # (b, n, d)
+        # (b, n, d) -> (b, n, d_graph_0) by padding zeros
         x = F.pad(x, (0, self.node_sizes[0][0] - self.input_node_size, 0, 0, 0, 0))
 
         for i in range(self.num_mps):
-            metric = _get_metric_func(self.metric if x.shape[-1] == 4 else 'cartesian')
-            A = self._getA(x, batch_size, self.input_edge_sizes[i], hidden_node_size=self.node_sizes[i][0], metric=metric)
+            metric = _get_metric_func(self.metric if x.shape[-1] == 4 else 'euclidean')
+            # Aij = xi ⊕ xj ⊕ d(xi, xj)
+            A = self._getA(
+                x=x, 
+                input_edge_size=self.input_edge_sizes[i], 
+                hidden_node_size=self.node_sizes[i][0],
+                metric=metric
+            )
+            # A = EdgeNet(A)
             A = self._edge_conv(A, i)
-            x = self._aggregate(x, A, i, batch_size)
+            
+            # xi = NodeNet(xi ⊕ sum_j Aij)
+            x = self._aggregate(x, A, i)
             x = x.view(batch_size, self.num_nodes, -1)
 
         x = x.view(batch_size, self.num_nodes, self.output_node_size)
@@ -175,12 +185,12 @@ class GraphNet(nn.Module):
     def _getA(
         self, 
         x: torch.Tensor, 
-        batch_size: int, 
         input_edge_size: int, 
         hidden_node_size: int, 
         metric: Callable
     ) -> torch.Tensor:
-        """Get Adjacency matrix A
+        """Get Adjacency matrix A, with
+        :math:`A_{ij} = x_i \oplus x_j \oplus d(x_i, x_j)`.
 
         :param x: node embeddings
         :type x: torch.Tensor
@@ -196,21 +206,22 @@ class GraphNet(nn.Module):
         Shape: (batch_size * self.num_nodes * self.num_nodes, input_edge_size)
         :rtype: torch.Tensor
         """
+        batch_size = x.shape[0]
         x1 = x.repeat(1, 1, self.num_nodes).view(batch_size, self.num_nodes*self.num_nodes, hidden_node_size)
         x2 = x.repeat(1, self.num_nodes, 1)  # 1*(self.num_nodes)*1 tensor with repeated x along axis=1
+        # adjacency matrix: D_{ij} = d(x_i, x_j)
         dists = metric(x2 - x1 + self.eps)
-        A = torch.cat((x1, x2, dists), 2).view(batch_size*self.num_nodes*self.num_nodes, input_edge_size)
+        # A_{ij} = x_i ⊕ x_j ⊕ d(x_i, x_j)
+        A = torch.cat((x1, x2, dists), 2).view(batch_size, self.num_nodes, self.num_nodes, input_edge_size)
         return A
 
     def _concat(
         self, 
         A: torch.Tensor, 
-        x: torch.Tensor, 
-        batch_size: int, 
-        edge_size: int, 
-        node_size: int
+        x: torch.Tensor
     ) -> torch.Tensor:
-        """Concatenate node features and edge features
+        """Concatenate node features and edge features: 
+        :math:`x \leftarrow x_i \oplus \sum_j A_{ij}`.
 
         :param A: Adjacency matrix that stores distances among nodes.
         :type A: torch.Tensor
@@ -224,21 +235,22 @@ class GraphNet(nn.Module):
         :type node_size: int
         :return: Concatenated edge and node features.
         :rtype: torch.Tensor
-        """        
-        A = A.view(batch_size, self.num_nodes, self.num_nodes, edge_size)
-        A = torch.sum(A, 2)
-        x = torch.cat((A, x), 2)
-        x = x.view(batch_size * self.num_nodes, edge_size + node_size)
+        """
+        # e_i = \sum_j A_{ij}
+        e = torch.sum(A, dim=-2)
+        # x_i <- x_i ⊕ e_i
+        # x = torch.cat((x, e), dim=-1)  # shape: (b, n, e+n) 
+        x = torch.cat((e, x), dim=-1)  # shape: (b, n, e+n) 
         return x
 
     def _aggregate(
         self, 
         x: torch.Tensor, 
         A: torch.Tensor, 
-        i: int, 
-        batch_size: int
+        i: int
     ) -> torch.Tensor:
         """Aggregate node features with edge features
+        :math:`x_j \leftarrow \mathrm{NodeNet}_i (x_j \oplus \sum_l A_{j k})`.
 
         :param x: Node embeddings.
         :type x: torch.Tensor
@@ -251,7 +263,8 @@ class GraphNet(nn.Module):
         :return: Aggregated node embeddings.
         :rtype: torch.Tensor
         """
-        x = self._concat(A, x, batch_size, self.edge_sizes[i][-1], self.node_sizes[i][0])
+        # x_i <- x_i ⊕ (\sum_j A_{ij})
+        x = self._concat(A, x)
         for j in range(len(self.node_net[i])):
             x = self.node_net[i][j](x)
             x = F.leaky_relu(x, negative_slope=self.alphas[i])
@@ -264,15 +277,16 @@ class GraphNet(nn.Module):
         A: torch.Tensor, 
         i: int
     ) -> torch.Tensor:
-        """Edge convolution at layer i
+        """Edge convolution at layer i:
+        :math:`A_{j k} \leftarrow \mathrm{EdgeNet}_i (A_{j k})`.
 
         :param A: Adjacency matrix that stores distances among nodes.
         :type A: torch.Tensor
         :param i: Layer index.
         :type i: int
-        :return: Convolved adjacency matrix.
+        :return: adjacency matrix after convolution.
         :rtype: torch.Tensor
-        """        
+        """
         for j in range(len(self.edge_net[i])):
             A = self.edge_net[i][j](A)
             A = F.leaky_relu(A, negative_slope=self.alphas[i])
@@ -298,29 +312,12 @@ def _create_dnn(
 
 
 def _adjust_var_list(data, num):
-    try:
+    if isinstance(data, Iterable):
         if len(data) < num:
             data = data + [data[-1]] * (num - len(data))
-            return data
-    except TypeError:  # Non interable
+    else:
         data = [data] * num
     return data[:num]
-
-# def _adjust_var_list(
-#     data: Union[List[List[Any]], List[Any], Any], 
-#     num: int
-# ) -> List[List[Any]]:
-#     if isinstance(data, list):
-#         if isinstance(data[0], list):
-#             # list of list
-#             if len(data) < num:
-#                 data = data + [data[-1]] * (num - len(data))
-#             else:
-#                 data = data[:num]
-#             return data
-#         else:
-#             # list -> list of list
-#             return [data] * num
 
 
 def _get_metric_func(metric: Union[Callable, str]) -> Callable:
