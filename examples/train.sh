840bd1e93a80a3fe92c661d0edd095d6dc85641b
Zichun Hao <zichunh2000@gmail.com>
2022-12-11 17:52:35 -0800

    Update example training script

--- examples/train.sh
+++ examples/train.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+set -xe
 
 mkdir -p "dev";
 python train.py \
 --batch-size 16 \
 --jet-type qcd \
 --data-paths "data/g_jets_30p_small.pt" "data/q_jets_30p_small.pt" \
+--test-data-paths "data/g_jets_30p_small.pt" "data/q_jets_30p_small.pt" \
 --encoder-edge-sizes '16,16,8,8;' \
 --decoder-edge-sizes '16,16,8,8;' \
 --encoder-node-sizes '3;3;3;3;' \
 --decoder-node-sizes '3;3;3;3;' \
 --save-dir dev \
 --num-epochs 10 \
---latent-map "local mix" \
---latent-node-size 2
---train-fraction 0.65 \
+--latent-map "mean" \
+--latent-node-size 2 \
 --save-dir dev \
 | tee -a dev/autoencoder-g-s1-v1.txt
